b9f10f99d505b22b5f2f8408636822b0c460bf6b
Joshna Jose <jishmalu@gmail.com>
2024-08-11 19:51:57 +0100

    Code added to git

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+﻿var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+$(window).on('load resize', function(){
+    var headerHeight = $('.header img').height(); 
+    $('.header').height(headerHeight); 
+});
